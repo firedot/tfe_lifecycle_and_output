e236ed0c74a87dba6e2ac6363af150b15c5d9312
Alexander Todorov <alext@Alexanders-MacBook-Pro.local>
2018-11-27 22:31:51 +0200

    Updated scripts/ and templates/

--- scripts/tfe_api_manage.sh
+++ scripts/tfe_api_manage.sh
@@ -13,23 +13,41 @@ export ORG_NAME=${ORG_NAME}
 export WSPACE_NAME=${WSPACE_NAME}
 export TOKEN=${TOKEN}
 export TFE=https://app.terraform.io/api/v2
+export WSPACE_ID="$(curl \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --header "Content-Type: application/vnd.api+json" \
+  ${TFE}/organizations/${ORG_NAME}/workspaces/${WSPACE_NAME} 2>/dev/null | jq '.data | .id' | tr -d \")"
 
 echo -e "\nPlease choose an operation from the list below: \n \
     1) Run and apply operation \n \
     2) Get variable info \n \
     3) \n \
     4) \n \
+    5) Exit \n
 \n \
     Please enter a number in the range 1-4 that corresponds to your choice or enter 5 to exit \n \
    "
 read -p 'Please choose option 1-4, or 5 to exit: ' CHOICE
-
-case ${CHOICE} in
-    "1") echo -e "\nYou chose a run and apply operationi \n"
-         bash /vagrant/scripts/run_plan_apply.sh;;
-    "2") echo -e "\nYou chose 'Get variable info' option \n"
-         bash /vagrant/scripts/tfe_var_manage.sh;;
-    "5") echo -e "\nGoodbye! Please have a nice day.\n";;
-
-
-esac
+while true; do
+    if [ ${CHOICE} == 1 ]; then
+         echo -e "\nYou chose a run and apply operationi \n"
+         bash /vagrant/scripts/run_plan_apply.sh
+         break
+    elif [ ${CHOICE} == 2 ]; then
+         echo -e "\nYou chose 'Get variable info' option \n"
+         bash /vagrant/scripts/tfe_var_manage.sh
+         break
+    elif [ ${CHOICE} == 3 ]; then
+         echo -e "\nYou chose \n"
+         break
+    elif [ ${CHOICE} == 4 ]; then
+         echo -e "\nYou chose \n"
+         break
+    elif [ ${CHOICE} == 5 ]; then
+         echo -e "\nGoodbye! Please have a nice day.\n"
+         break
+    else
+         echo -e "\n INVALID INPUT\n"
+         continue
+    fi
+done
--- scripts/tfe_var_manage.sh
+++ scripts/tfe_var_manage.sh
@@ -1,19 +1,20 @@
+#!/usr/bin/env bash
+
 # The following  block of code checks what variables are enabled for TFE Workspace
 
 CONFIRM_DESTROY_VALUE="$(curl \
   --header "Authorization: Bearer $TOKEN" \
   --header "Content-Type: application/vnd.api+json" \
   "${TFE}/vars?filter%5Borganization%5D%5Bname%5D=${ORG_NAME}&filter%5Bworkspace%5D%5Bname%5D=${WSPACE_NAME}" 2>/dev/null | \
-  jq ' .data | .[] | select(.attributes.key == "CONFIRM_DESTROY") | .attributes.value' | \
-  tr -d \" )"
-
-if  [ ${CONFIRM_DESTROY_VALUE}  -eq  1 ] ; then
-    echo "Queue destroy option is enabled"
-else
-    echo -e "Queue destroy option is disabled\n"
+  jq ' .data | .[] | select(.attributes.key == "CONFIRM_DESTROY") | .attributes.value' \
+)"
+echo ${CONFIRM_DESTROY_VALUE}
+if [ -z ${CONFIRM_DESTROY_VALUE} ]; then
+    echo -e "\nQueue destroy option is disabled\n"
     while read -p "Do you want to enable it: Y/N " ANSWER; do
-        if [ ${ANSWER} ==  'Y' ] || [ ${ANSWER} == 'y'  ]; then
+        if [ ${ANSWER} == 'Y' ] || [ ${ANSWER} == 'y' ]; then
              echo -e "\nThe CONFIRM_DESTROY ENV var will be set to 1 (enabled)\n"
+             bash /vagrant/scripts/enable_confirm_destroy.sh
              break
          elif [ ${ANSWER} == 'N' ] || [ ${ANSWER} == 'n' ]; then
               echo -e "\nNo changes were made.\n"
@@ -25,4 +26,8 @@ in  order for destroy operations to be possible\n"
              continue
         fi
     done
+elif [ ${CONFIRM_DESTROY_VALUE} == '"1"' ]; then
+    echo -e "\nQueue destroy option is enabled\n"
+else
+    echo -e "\nFAILURE: Something is not right\n"
 fi
