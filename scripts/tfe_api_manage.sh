925c582a6b7bacf6e33067e0efe2feb97a59924e
Alexander Todorov <alext@Alexanders-MacBook-Pro.local>
2018-11-27 10:50:29 +0200

    Added a script that reads input for Organization and Workspace names, as well as API user token

---
+++ scripts/tfe_api_manage.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+read -p 'Please enter your organization name: ' ORG_NAME
+read -p 'Please enter your workspace name: ' WSPACE_NAME
+read -p 'Please enter your API user token: ' TOKEN
+
+export TOKEN=${TOKEN}
+
+
